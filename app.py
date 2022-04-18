192018cf4ffb86ac2aea3b84032bfc73a120ba0a
Miandic <misha.andreev.novo@gmail.com>
2022-04-18 14:58:52 +0300

    minimal flask

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ def index(name = None):
     return render_template('index.html', name=name)
 
 
+@app.route('/user')
+def user(name = None):
+    return render_template('profile.html', name=name)
+
+
+@app.route('/reg')
+def registr(name = None):
+    return render_template('reg.html', name=name)
+
+
+@app.route('/upload')
+def upload(name = None):
+    return render_template('upload.html', name=name)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
