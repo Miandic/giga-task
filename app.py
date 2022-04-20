0dc7def0604161c3a2f5df098e7fee6f31ec80ae
jpegMushrum <103191629+jpegMushrum@users.noreply.github.com>
2022-04-20 23:57:46 +0300

    ы

--- app.py
+++ app.py
@@ -15,15 +15,9 @@ conn, cur = functions.set_connection(conn , cur)
 
 
 @app.route('/')
-<<<<<<< HEAD
-def index(name = None, nick=None):
-    #проверка по кукам что аккаунт войдён
-    #если нет, то 5
-=======
 def index(name=None, nick=None, create='true', other = None):
-    #проверка по кукуам что аккаунт войдён
+    #проверка по кукам что аккаунт войдён
     #если нет, то
->>>>>>> 714c64d2ac6ed68ba7355b2112940ee78e4cb718
     if (request.cookies.get('login') != None):
         name = request.cookies.get('login')
         users = functions.get_users(conn, cur)
