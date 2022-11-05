0799a670629a48b0aba771f54dbecf0179df5d92
Miandic <misha.andreev.novo@gmail.com>
2022-11-05 14:57:47 +0300

    Updates

---
+++ precache-manifest.9b01b455b1448ed3c7c269c44650646e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc683c50f2aee69f7fe1",
+    "url": "/static/css/main.47938a7d.chunk.css"
+  },
+  {
+    "revision": "bc683c50f2aee69f7fe1",
+    "url": "/static/js/main.bc683c50.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "5fc9f08acb781dce442d",
+    "url": "/static/js/2.5fc9f08a.chunk.js"
+  },
+  {
+    "revision": "f8403e306dc7e4031e5b",
+    "url": "/static/js/runtime~main.f8403e30.js"
+  },
+  {
+    "revision": "65c08e51c64c2b5334ca6cc955d0d995",
+    "url": "/index.html"
+  }
+];
