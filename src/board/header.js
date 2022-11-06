a56927f93eeb7ec62b640f52a78570c70fca99a7
Miandic <misha.andreev.novo@gmail.com>
2022-11-06 11:20:11 +0300

    Local logo

--- src/board/header.js
+++ src/board/header.js
@@ -4,7 +4,7 @@ export default function Header({board, onChange}) {
     return (
         <div className='header'>
             <div className='header-logo'>
-                <img src="https://downloader.disk.yandex.ru/preview/0621fab39abb84ba2c5d13c280be4125246da4758a8100bea80ba46f8d8b005d/63667dca/s3qLNl5FsEz_dos1HAcFbUrL_DHAMbn1EsbyLfGuPDsapdTTPitG-3aM-D3RT7vE-mqR3xPcHJSOQWQ5tPzo3A%3D%3D?uid=0&filename=ggt-logo.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v2&size=2048x2048" alt="#" />
+                <img src="ggt-logo.png" alt="#" />
                 <span>GigaTask</span>
             </div>
             <input type="text" name="board-name" value={board.name} onChange = {e => onChange(e.target.value)} />
